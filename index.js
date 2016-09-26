c0164d92f594594b7ad18b4f97b7ec2e8adea29d
Raphaël PARE <rapha.pare@gmail.com>
2016-09-26 18:28:46 +0200

    Polishing

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+// CONF
+var port = 4000;
+// END CONF
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.listen(4000);
-console.log('server is running');
+app.listen(port);
+console.log('server is running on port :' + port);
 
 app.get('/', function(req, res) {
   res.render('index');
