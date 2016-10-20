098373ad5a5fa0b70a427b8586820b030f684d23
Raphaël PARE <rapha.pare@gmail.com>
2016-10-20 21:47:50 +0200

    Base angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,34 @@
-angular.module('bookingApp', []);
+var app = angular.module('bookingApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+
+  .when('/', {
+    templateUrl : 'pages/home.html',
+    controller  : 'HomeController'
+  })
+
+  .when('/blog', {
+    templateUrl : 'pages/blog.html',
+    controller  : 'BlogController'
+  })
+
+  .when('/about', {
+    templateUrl : 'pages/about.html',
+    controller  : 'AboutController'
+  })
+
+  .otherwise({redirectTo: '/'});
+});
+
+app.controller('HomeController', function($scope) {
+  $scope.message = 'Hello from HomeController';
+});
+
+app.controller('BlogController', function($scope) {
+  $scope.message = 'Hello from BlogController';
+});
+
+app.controller('AboutController', function($scope) {
+  $scope.message = 'Hello from AboutController';
+});
